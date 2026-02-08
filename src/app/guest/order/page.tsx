a9e879f856bb1d765d99cf5d6a748c88c7329f93
fabian nugrah <fabianugerah@gmail.com>
2026-02-08 19:17:24 +0700

    Update Responsive Table

--- src/app/guest/order/page.tsx
+++ src/app/guest/order/page.tsx
@@ -297,7 +297,7 @@ export default function CustomerOrderPage() {
                       </div>
                     ) : (
                       <>
-                        <div className="grid grid-cols-4 sm:grid-cols-6 gap-3 mb-4">
+                        <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-6  gap-3 mb-4">
                           {tables.map((table) => (
                             <button
                               key={table.no_meja}
